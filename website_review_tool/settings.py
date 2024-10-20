9f4ab1e257038a54dcf8ed2d1af633155bb2e3c3
jazheng <jazheng@student.unimelb.edu.au>
2024-10-20 23:53:49 +1100

    made static root and staticfiles dirs point to seperate directories

--- website_review_tool/settings.py
+++ website_review_tool/settings.py
@@ -150,12 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if not DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR,'static'),
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
