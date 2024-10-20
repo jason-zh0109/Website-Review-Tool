d03ff1f5b298f1c07c37e6c96624e9d191385f63
jazheng <jazheng@student.unimelb.edu.au>
2024-10-20 20:03:39 +1100

    removed staticfiles storage

--- website_review_tool/settings.py
+++ website_review_tool/settings.py
@@ -154,7 +154,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
